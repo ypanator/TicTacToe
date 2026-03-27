b95ffe7b10f533e341496c5a887d7c6b9e3f6b4a
ypanator <153828092+ypanator@users.noreply.github.com>
2026-03-27 08:48:30 +0000

    Add reactive cells

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [cellValues, setCellValues] = useState([]);
+  const [cellValues, setCellValues] = useState(
+    [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "]
+    ]
+  );
+  const [curSymbol, setCurSymbol] = useState("X");
 
-  function OnCellClick(cell) {
-    console.log(cell);
+  function switchCurSymbol() {
+    (curSymbol === "X" ? setCurSymbol("O") : setCurSymbol("X"));
+  }
+
+  function OnCellClick(id) {
+    setCellValues((prev) => {
+      const newCellValues = [...prev];
+      newCellValues[Math.floor(id / 3)][id % 3] = curSymbol;
+      return newCellValues;
+    });
+    switchCurSymbol();
   }
 
   return <>
   <div className="GameBoard">
-    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((cell) => (
+    {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((cell) => (
       <div 
         className="GameBoardCell" 
         key={cell} 
