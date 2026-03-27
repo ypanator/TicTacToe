3a3849aa15566a85401d4dc9ffa44678118d6fce
ypanator <153828092+ypanator@users.noreply.github.com>
2026-03-27 12:15:20 +0000

    State logic cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,51 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const PLAYING = 0, WONX = 1, WONO = 2, DRAW = 3;
-  const emptyCells = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-  ]
+  const PLAYING = 0, WON_X = 1, WON_O = 2, DRAW = 3;
+  const X = "X", O = "O", EMPTY = " ";
   
+  const [cellValues, setCellValues] = useState(
+    [
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY]
+    ]
+  );
+  const [curSymbol, setCurSymbol] = useState(X);
+  const [gameState, setGameState] = useState(PLAYING);
+  const [infoBoxRender, setInfoBoxRender] = useState(false);
+  const [infoBoxText, setInfoBoxText] = useState("");
 
-  const [cellValues, setCellValues] = useState(structuredClone(emptyCells));
-  const [curSymbol, setCurSymbol] = useState("X");
-  const [state, setState] = useState(PLAYING);
+  // aparently useEffect is not necessary here *shrug*
+  if (gameState === PLAYING) {
+    setInfoBoxRender(false)
+  } else {
+    setInfoBoxRender(true);
+    if (gameState === WON_O) {
+      setInfoBoxText("O wins");
+    }
+    if (gameState === WON_X) {
+      setInfoBoxText("X wins");
+    }
+    if (gameState === DRAW) {
+      setInfoBoxText("Draw");
+    }
+  }
+
+  function restart() {
+    setCellValues(
+      [
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
+      ]
+    );
+    setGameState(PLAYING);
+    setCurSymbol(X);
+  }
 
   function switchCurSymbol() {
-    (curSymbol === "X" ? setCurSymbol("O") : setCurSymbol("X"));
+    (curSymbol === X ? setCurSymbol(O) : setCurSymbol(X));
   }
 
   function calcState(symbol) {
@@ -29,7 +60,7 @@ function App() {
         cellValues[1][i] === symbol && 
         cellValues[2][i] === symbol
       )) {
-        return symbol === "X" ? WONX : WONO;
+        return symbol === X ? WON_X : WON_O;
       }
     }
 
@@ -42,10 +73,10 @@ function App() {
       cellValues[1][1] === symbol && 
       cellValues[2][0] === symbol
     )) {
-      return symbol === "X" ? WONX : WONO;
+      return symbol === X ? WON_X : WON_O;
     }
 
-    if (cellValues.flat().filter(x => x === " ").length === 0) {
+    if (cellValues.flat().filter(x => x === EMPTY).length === 0) {
       return DRAW;
     }
 
@@ -53,13 +84,16 @@ function App() {
   }
 
   function OnCellClick(id) {
+    if (gameState !== PLAYING) return;
+
     setCellValues((prev) => {
       const newCellValues = [...prev];
       newCellValues[Math.floor(id / 3)][id % 3] = curSymbol;
       return newCellValues;
     });
+
     switchCurSymbol();
-    setState(calcState(curSymbol));
+    setGameState(calcState(curSymbol));
   }
 
   return <>
@@ -76,16 +110,10 @@ function App() {
     ))}
   </div>
   {
-    (state === WONO || state === WONX || state === DRAW) && <div>
-      {state === DRAW ? "Draw" : state === WONO ? "O wins" : "X wins"}
-    </div>
+    infoBoxRender && <div className="InfoBox">{infoBoxText}</div>
   }
-  <button onClick={() => {
-    setCellValues(structuredClone(emptyCells));
-    setState(PLAYING);
-    setCurSymbol("X");
-  }}>
-    reset
+  <button onClick={restart}>
+    restart
   </button>
   </>
 }
