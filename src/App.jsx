a1a27cd503ccb24322958594821964294aca36f6
ypanator <153828092+ypanator@users.noreply.github.com>
2026-03-27 09:37:05 +0000

    Implement state logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,54 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [cellValues, setCellValues] = useState(
-    [
-      [" ", " ", " "],
-      [" ", " ", " "],
-      [" ", " ", " "]
-    ]
-  );
+  const PLAYING = 0, WONX = 1, WONO = 2, DRAW = 3;
+  const emptyCells = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+  ]
+  
+
+  const [cellValues, setCellValues] = useState(structuredClone(emptyCells));
   const [curSymbol, setCurSymbol] = useState("X");
+  const [state, setState] = useState(PLAYING);
 
   function switchCurSymbol() {
     (curSymbol === "X" ? setCurSymbol("O") : setCurSymbol("X"));
+  }
+
+  function calcState(symbol) {
+    for (let i = 0; i < 3; i++) {
+      if ((
+        cellValues[i][0] === symbol && 
+        cellValues[i][1] === symbol && 
+        cellValues[i][2] === symbol
+      ) || (
+        cellValues[0][i] === symbol && 
+        cellValues[1][i] === symbol && 
+        cellValues[2][i] === symbol
+      )) {
+        return symbol === "X" ? WONX : WONO;
+      }
+    }
+
+    if ((
+      cellValues[0][0] === symbol && 
+      cellValues[1][1] === symbol && 
+      cellValues[2][2] === symbol
+    ) || (
+      cellValues[0][2] === symbol && 
+      cellValues[1][1] === symbol && 
+      cellValues[2][0] === symbol
+    )) {
+      return symbol === "X" ? WONX : WONO;
+    }
+
+    if (cellValues.flat().filter(x => x === " ").length === 0) {
+      return DRAW;
+    }
+
+    return PLAYING;
   }
 
   function OnCellClick(id) {
@@ -22,6 +59,7 @@ function App() {
       return newCellValues;
     });
     switchCurSymbol();
+    setState(calcState(curSymbol));
   }
 
   return <>
@@ -37,7 +75,18 @@ function App() {
       </div>
     ))}
   </div>
-  <div className="test"></div>
+  {
+    (state === WONO || state === WONX || state === DRAW) && <div>
+      {state === DRAW ? "Draw" : state === WONO ? "O wins" : "X wins"}
+    </div>
+  }
+  <button onClick={() => {
+    setCellValues(structuredClone(emptyCells));
+    setState(PLAYING);
+    setCurSymbol("X");
+  }}>
+    reset
+  </button>
   </>
 }
 
